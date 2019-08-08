7675234e5199a42cfc29f5940a3fafc0d71c69b6
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-08 22:26:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 programmer_hash.unshift[:yukihiro_matsumoto][:known for => "Ruby", :languages => ["LISP", "C"]]
+programmer_hash[:yukihiro_matsumoto]
 	programmer_hash =
  		{
         :grace_hopper => {
