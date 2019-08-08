2f1e385fbb4f06988c1966b565113e064360a90b
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-08 22:27:29 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -65,6 +65,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 programmer_hash.unshift[:yukihiro_matsumoto][:known for => "Ruby", :languages => ["LISP", "C"]]
+programmer_hash[:yukihiro_matsumoto]
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -103,6 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for] = "alans_new_info"
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:alans_new_info]
 
 
 end
